abe23a767e820cc94017c8f8720890ec25861e76
MJafar777 <mirzaraximovjafar2001@gmail.com>
2023-05-05 11:37:42 +0500

    this project build in typeScript

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 import { IData } from "../interfaces";
 
 export const data: IData[] = [
-  { title: "Alex", id: 1, description: "hello my name Alex" },
-  { title: "anna", id: 2, description: "hello my name anna" },
-  { title: "doe", id: 3, description: "hello my name doe" },
+  // { title: "Alex", id: 1, description: "hello my name Alex" },
+  // { title: "anna", id: 2, description: "hello my name anna" },
+  // { title: "doe", id: 3, description: "hello my name doe" },
 ];
